7c74f2b3cb3434c9ef5b63b13dece09e156cfbd7
Ataberk Canıtez <aticanitez@hotmail.com>
2020-09-01 16:30:40 +0300

    final commit

---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+class Question{
+
+  String questionText;
+  bool questionAnswer;
+
+
+  Question(String question, bool answer){
+    this.questionAnswer = answer;
+    this.questionText = question;
+  }
+
+}
